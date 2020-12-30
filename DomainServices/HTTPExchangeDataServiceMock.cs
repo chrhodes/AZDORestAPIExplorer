e9dee5be082f10d710c6b49ba5af3bdd49363728
Christopher Rhodes <chrhodes@cox.net>
2020-12-30 07:23:59 -0800

    Add HTTPExchange

---
+++ DomainServices/HTTPExchangeDataServiceMock.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+using AZDORestApiExplorer.Domain;
+
+using VNC;
+using VNC.Core.DomainServices;
+
+namespace AZDORestApiExplorer.DomainServices
+{
+    public class HTTPExchangeDataServiceMock : IHTTPExchangeDataService
+    {
+        public IEnumerable<HTTPExchange> All()
+        {
+            Int64 startTicks = Log.DOMAINSERVICES("Enter", Common.LOG_APPNAME);
+
+            // TODO(crhodes)
+            // Load data from real database.
+            // For now just return hard coded list.
+
+            yield return new HTTPExchange
+            {
+                Id = 1,
+                FieldString = "FieldString",
+                FieldDouble = 2.0,
+                FieldInt = 23
+
+            };
+
+            yield return new HTTPExchange
+            {
+                Id = 2,
+                FieldString = null,
+                FieldDouble = Double.MaxValue,
+                FieldInt = int.MaxValue
+            };
+
+            Log.DOMAINSERVICES("Exit", Common.LOG_APPNAME, startTicks);
+        }
+
+        public Task<List<HTTPExchange>> AllAsync()
+        {
+            Int64 startTicks = Log.DOMAINSERVICES("Enter", Common.LOG_APPNAME);
+
+            throw new NotImplementedException();
+
+            Log.DOMAINSERVICES("Exit", Common.LOG_APPNAME, startTicks);
+        }
+
+        public IEnumerable<HTTPExchange> AllInclude(params Expression<Func<HTTPExchange, object>>[] includeProperties)
+        {
+            Int64 startTicks = Log.DOMAINSERVICES("Enter", Common.LOG_APPNAME);
+
+            throw new NotImplementedException();
+
+            Log.DOMAINSERVICES("Exit", Common.LOG_APPNAME, startTicks);
+        }
+
+        public Task<IEnumerable<HTTPExchange>> AllIncludeAsync(params Expression<Func<HTTPExchange, object>>[] includeProperties)
+        {
+            Int64 startTicks = Log.DOMAINSERVICES("Enter", Common.LOG_APPNAME);
+
+            throw new NotImplementedException();
+
+            Log.DOMAINSERVICES("Exit", Common.LOG_APPNAME, startTicks);
+        }
+
+        public void Delete(int entityId)
+        {
+            Int64 startTicks = Log.DOMAINSERVICES("Enter", Common.LOG_APPNAME);
+
+            throw new NotImplementedException();
+
+            Log.DOMAINSERVICES("Exit", Common.LOG_APPNAME, startTicks);
+        }
+
+        public void DeleteAsync(int entityId)
+        {
+            Int64 startTicks = Log.DOMAINSERVICES("Enter", Common.LOG_APPNAME);
+
+            throw new NotImplementedException();
+
+            Log.DOMAINSERVICES("Exit", Common.LOG_APPNAME, startTicks);
+        }
+
+        public IEnumerable<HTTPExchange> FindBy(Expression<Func<HTTPExchange, bool>> predicate)
+        {
+            Int64 startTicks = Log.DOMAINSERVICES("Enter", Common.LOG_APPNAME);
+
+            throw new NotImplementedException();
+
+            Log.DOMAINSERVICES("Exit", Common.LOG_APPNAME, startTicks);
+        }
+
+        public Task<IEnumerable<HTTPExchange>> FindByAsync(Expression<Func<HTTPExchange, bool>> predicate)
+        {
+            Int64 startTicks = Log.DOMAINSERVICES("Enter", Common.LOG_APPNAME);
+
+            throw new NotImplementedException();
+
+            Log.DOMAINSERVICES("Exit", Common.LOG_APPNAME, startTicks);
+        }
+
+        public HTTPExchange FindById(int entityId)
+        {
+            Int64 startTicks = Log.DOMAINSERVICES("Enter", Common.LOG_APPNAME);
+
+            throw new NotImplementedException();
+
+            Log.DOMAINSERVICES("Exit", Common.LOG_APPNAME, startTicks);
+        }
+
+        public Task<HTTPExchange> FindByIdAsync(int entityId)
+        {
+            Int64 startTicks = Log.DOMAINSERVICES("Enter", Common.LOG_APPNAME);
+
+            throw new NotImplementedException();
+
+            Log.DOMAINSERVICES("Exit", Common.LOG_APPNAME, startTicks);
+        }
+
+        public IEnumerable<HTTPExchange> FindByInclude(Expression<Func<HTTPExchange, bool>> predicate, params Expression<Func<HTTPExchange, object>>[] includeProperties)
+        {
+            Int64 startTicks = Log.DOMAINSERVICES("Enter", Common.LOG_APPNAME);
+
+            throw new NotImplementedException();
+
+            Log.DOMAINSERVICES("Exit", Common.LOG_APPNAME, startTicks);
+        }
+
+        public Task<IEnumerable<HTTPExchange>> FindByIncludeAsync(Expression<Func<HTTPExchange, bool>> predicate, params Expression<Func<HTTPExchange, object>>[] includeProperties)
+        {
+            Int64 startTicks = Log.DOMAINSERVICES("Enter", Common.LOG_APPNAME);
+
+            throw new NotImplementedException();
+
+            Log.DOMAINSERVICES("Exit", Common.LOG_APPNAME, startTicks);
+        }
+
+        public bool HasChanges()
+        {
+            Int64 startTicks = Log.DOMAINSERVICES("Enter", Common.LOG_APPNAME);
+
+            throw new NotImplementedException();
+
+            Log.DOMAINSERVICES("Exit", Common.LOG_APPNAME, startTicks);
+        }
+
+        public void Insert(HTTPExchange entity)
+        {
+            Int64 startTicks = Log.DOMAINSERVICES("Enter", Common.LOG_APPNAME);
+
+            throw new NotImplementedException();
+
+            Log.DOMAINSERVICES("Exit", Common.LOG_APPNAME, startTicks);
+        }
+
+        public Task<HTTPExchange> InsertAsync(HTTPExchange entity)
+        {
+            Int64 startTicks = Log.DOMAINSERVICES("Enter", Common.LOG_APPNAME);
+
+            throw new NotImplementedException();
+
+            Log.DOMAINSERVICES("Exit", Common.LOG_APPNAME, startTicks);
+        }
+
+        public void Update(HTTPExchange entity)
+        {
+            Int64 startTicks = Log.DOMAINSERVICES("Enter", Common.LOG_APPNAME);
+
+            throw new NotImplementedException();
+
+            Log.DOMAINSERVICES("Exit", Common.LOG_APPNAME, startTicks);
+        }
+
+        public Task<HTTPExchange> UpdateAsync(HTTPExchange entity)
+        {
+            Int64 startTicks = Log.DOMAINSERVICES("Enter", Common.LOG_APPNAME);
+
+            throw new NotImplementedException();
+
+            Log.DOMAINSERVICES("Exit", Common.LOG_APPNAME, startTicks);
+        }
+
+        public Task UpdateAsync()
+        {
+            Int64 startTicks = Log.DOMAINSERVICES("Enter", Common.LOG_APPNAME);
+
+            throw new NotImplementedException();
+
+            Log.DOMAINSERVICES("Exit", Common.LOG_APPNAME, startTicks);
+        }
+
+        Task IDataService<HTTPExchange>.DeleteAsync(int entityId)
+        {
+            Int64 startTicks = Log.DOMAINSERVICES("Enter", Common.LOG_APPNAME);
+
+            throw new NotImplementedException();
+
+            Log.DOMAINSERVICES("Exit", Common.LOG_APPNAME, startTicks);
+        }
+
+        Task IDataService<HTTPExchange>.InsertAsync(HTTPExchange entity)
+        {
+            Int64 startTicks = Log.DOMAINSERVICES("Enter", Common.LOG_APPNAME);
+
+            throw new NotImplementedException();
+
+            Log.DOMAINSERVICES("Exit", Common.LOG_APPNAME, startTicks);
+        }
+
+        Task IDataService<HTTPExchange>.UpdateAsync(HTTPExchange entity)
+        {
+            Int64 startTicks = Log.DOMAINSERVICES("Enter", Common.LOG_APPNAME);
+
+            throw new NotImplementedException();
+
+            Log.DOMAINSERVICES("Exit", Common.LOG_APPNAME, startTicks);
+        }
+    }
+}
---
+++ DomainServices/Interfaces/IHTTPExchangeDataService.cs
@@ -0,0 +1,11 @@
+﻿using AZDORestApiExplorer.Domain;
+
+using VNC.Core.DomainServices;
+
+namespace AZDORestApiExplorer.DomainServices
+{
+    public interface IHTTPExchangeDataService : IGenericRepository<HTTPExchange>
+    {
+        void RemovePhoneNumber(HTTPExchangePhoneNumber model);
+    }
+}
