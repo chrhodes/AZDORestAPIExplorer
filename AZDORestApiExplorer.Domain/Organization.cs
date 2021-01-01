1f5427224e8ee893067f226c8aad50e27f950b14
Christopher Rhodes <chrhodes@cox.net>
2020-12-31 18:34:06 -0800

    Missed a few files

---
+++ AZDORestApiExplorer.Domain/Organization.cs
@@ -0,0 +1,15 @@
+﻿namespace AZDORestApiExplorer.Domain
+{
+
+    public class AvailableCollection
+    {
+        public string Name { get; set; }
+        public Organization Organization { get; set; }
+    }
+
+    public class Organization
+    {
+        public string Uri { get; set; }
+        public string PAT { get; set; }
+    }
+}
---
+++ AZDORestApiExplorer.Domain/SystemControl.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using VNC.Core.DomainServices;
+
+namespace AZDORestApiExplorer.Domain
+{
+    public class SystemControl
+    {
+    }
+    // TODO(crhodes)
+    // PasteSpecial from Json result text
+
+    // Rename classes to use TYPEsRoot and TYPE classes
+
+}
