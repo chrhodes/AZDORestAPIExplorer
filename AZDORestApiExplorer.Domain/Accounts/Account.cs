b541d5224b74b69f8fda8928da9d7900f806c6b4
Christopher Rhodes <chrhodes@cox.net>
2021-01-01 10:14:36 -0800

    A lot of structure to reflect RestAPI categories.  Hope I don't regret this.

---
+++ AZDORestApiExplorer.Domain/Accounts/Account.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using VNC.Core.DomainServices;
+
+namespace AZDORestApiExplorer.Domain.Accounts
+{
+    public class AccountsRoot
+    {
+    }
+    public class Account
+    {
+    }
+    // TODO(crhodes)
+    // PasteSpecial from Json result text
+
+    // Rename classes to use TYPEsRoot and TYPE classes
+
+}
